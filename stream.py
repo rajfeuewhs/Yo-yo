9f361ac553071a11f5076f44154a3639d43411b5
ARSHIT RAJ <rajarshit53@gmail.com>
2026-03-09 00:09:06 +0530

    Update stream.py

--- stream.py
+++ stream.py
@@ -5,15 +5,17 @@ def start_stream(stream_key):
     command = [
         "ffmpeg",
         "-re",
-        "-loop", "1",
-        "-i", "bg.png",
+        "-f", "lavfi",
+        "-i", "color=c=purple:s=1280x720:r=30",
         "-f", "lavfi",
         "-i", "anullsrc",
-        "-vcodec", "libx264",
+        "-c:v", "libx264",
         "-preset", "veryfast",
         "-pix_fmt", "yuv420p",
+        "-c:a", "aac",
+        "-b:a", "128k",
         "-f", "flv",
-        f"rtmp://a.rtmp.youtube.com/live2/{stream_key}"
+        f"rtmp://x.rtmp.youtube.com/live2/{stream_key}"
     ]
 
     subprocess.Popen(command)
